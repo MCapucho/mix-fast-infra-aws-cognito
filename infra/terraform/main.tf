d2168b201dc95471fb1f5c8c212cf4a9e1d90165
Murylo Capucho <mcapuchoribeiro@gmail.com>
2023-10-26 18:34:28 -0300

    Implementação da Infra Cognito

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,8 +1,8 @@
 resource "aws_cognito_user_pool" "mixfast_cognito_user_pool" {
   name = "${var.name}_user_pool"
 
-  username_attributes = ["email", "preferred_username"]
-  auto_verified_attributes = ["email", "preferred_username"]
+  username_attributes = ["email"]
+  auto_verified_attributes = ["email"]
 
   password_policy {
     minimum_length    = 8
@@ -16,6 +16,19 @@ resource "aws_cognito_user_pool" "mixfast_cognito_user_pool" {
     default_email_option = "CONFIRM_WITH_CODE"
     email_subject = "Account Confirmation"
     email_message = "Your confirmation code is {####}"
+  }
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "preferred_username"
+    required                 = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
   }
 
   schema {
